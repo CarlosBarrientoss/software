78864ffbd367a24303cd201ef3b00c0aa23ba42f
Carlosabs <carlosabarrientoss@gmail.com>
2025-04-07 15:08:53 -0500

    Mi primer commit, subiendo el proyecto

---
+++ app/vistas/modulos/personas.php
@@ -0,0 +1,95 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Administrar personas</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Administrar personas</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+
+      <div class="register-box" style="margin: 0 auto">
+          <div class="register-logo">
+            <a href="../../index2.html"><b>Admin</b>LTE</a>
+          </div>
+
+          <div class="card">
+            <div class="card-body register-card-body">
+              <p class="login-box-msg">Register a new membership</p>
+
+              <form action="../../index.html" method="post">
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Full name">
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-user"></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                  <input type="email" class="form-control" placeholder="Email">
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-envelope"></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                  <input type="password" class="form-control" placeholder="Password">
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="input-group mb-3">
+                  <input type="password" class="form-control" placeholder="Retype password">
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-8">
+                    <div class="icheck-primary">
+                      <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                      <label for="agreeTerms">
+                      I agree to the <a href="#">terms</a>
+                      </label>
+                    </div>
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block">Register</button>
+                  </div>
+                  <!-- /.col -->
+                </div>
+              </form>
+              <a href="login.html" class="text-center">I already have a membership</a>
+            </div>
+            <!-- /.form-box -->
+          </div><!-- /.card -->
+        </div>
+        <!-- /.register-box -->
+
+
+     
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+
+  </div>
